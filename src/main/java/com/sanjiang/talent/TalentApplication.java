27eec961ac4bbf45dd57e3bacc6dd95a80cdf8ce
mengfanchun <2290507460@qq.com>
2020-04-21 10:39:49 +0800

    专业课程

--- src/main/java/com/sanjiang/talent/TalentApplication.java
+++ src/main/java/com/sanjiang/talent/TalentApplication.java
@@ -3,15 +3,21 @@ package com.sanjiang.talent;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan("com.sanjiang.talent.mapper")
 @EnableTransactionManagement
-public class TalentApplication {
+public class TalentApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TalentApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TalentApplication.class);
+    }
 }
